0c57268a5bb678f98a9b4973c4f742cf8c0916a7
alanas404 <al.anas404040@gmail.com>
2025-03-21 23:50:47 +0600

    roles and permissions section card design done

---
+++ src/app/(dashboard)/roles-permissions/data.ts
@@ -0,0 +1,10 @@
+ export const roles = [
+    { id: 1, name: "Administrator" ,color:"#2465FF"},
+    { id: 2, name: "Manager",color:"#F5A623" },
+    { id: 3, name: "Sales",color:"#FF1A1A"},
+    { id: 4, name: "Support",color:"#8A63D2"},
+    { id: 5, name: "Developer",color:"#FF1A1A"},
+    { id: 6, name: "HR Department",color:"#11A849"},
+    { id: 7, name: "Restricted User",color:"#4E36F5"},
+    { id: 8, name: "Customer",color:"#0070F3"},
+  ];
--- src/app/(dashboard)/roles-permissions/page.tsx
+++ src/app/(dashboard)/roles-permissions/page.tsx
@@ -8,13 +8,10 @@ import {
 } from '@ant-design/icons';
 import AddNewUserModal from "./components/AddNewUserModal";
 import TableComponent from "../components/TableComponent";
+import { roles } from "./data";
 
 const Page = () => {
-  const roles = [
-    { id: 1, name: "Administrator" },
-    { id: 2, name: "Manager" },
-    { id: 3, name: "Sales" },
-  ];
+ 
   const [modal1Open, setModal1Open] = useState(false);
   const [modal2Open, setModal2Open] = useState(false);
   return (
@@ -22,9 +19,9 @@ const Page = () => {
       <h1 className="font-lexend-deca text-2xl font-bold">
         Roles & Permissions
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
         {roles.map((role) => (
-          <RoleCard key={role.id} setModal1Open={setModal1Open}/>
+          <RoleCard key={role.id} role={role} setModal1Open={setModal1Open}/>
         ))}
       </div>
       <Modal
