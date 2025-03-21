0c4083714ef51f3b3ca65ddfcc0a3d37914b8d12
alanas404 <al.anas404040@gmail.com>
2025-03-22 00:36:55 +0600

    role update

--- src/app/(dashboard)/roles-permissions/components/RoleModal.tsx
+++ src/app/(dashboard)/roles-permissions/components/RoleModal.tsx
@@ -1,14 +1,22 @@
+import { rolesPermissions } from "../data";
+
 const RoleModal = () => {
+
+    const lastLength = rolesPermissions.length - 1;
+
     return <div>
-        <p className="font-lexend-deca text-base font-semibold my-6">Role Access</p>
-        <div className="flex items-center justify-between pb-4 border-b">
-            <h1 className="font-lexend-deca text-base font-semibold text-[#333]">Administrator</h1>
-            <div className="flex items-center gap-3">
-                <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Read</button>
-                <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Write</button>
-                <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Delete</button>
-            </div>
-        </div>
+        <p className="font-lexend-deca text-base font-semibold mt-6">Role Access</p>
+        {
+            rolesPermissions.map((role,index) => ( <div className={`flex items-center justify-between py-4 ${index !== lastLength ? 'border-b' : ''}`} key={role.id}>
+                <h1 className="font-lexend-deca text-base font-semibold text-[#333]">{role.name}</h1>
+                <div className="flex items-center gap-3">
+                    <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Read</button>
+                    <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Write</button>
+                    <button className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-[90px] h-[40px]">Delete</button>
+                </div>
+            </div>))     
+        }
+        
     </div>
 }
 
--- src/app/(dashboard)/roles-permissions/data.ts
+++ src/app/(dashboard)/roles-permissions/data.ts
@@ -8,3 +8,15 @@
     { id: 7, name: "Restricted User",color:"#4E36F5"},
     { id: 8, name: "Customer",color:"#0070F3"},
   ];
+  
+  export const rolesPermissions = [
+    { id: 1, name: "Administrator", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 2, name: "Manager", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 3, name: "Sales", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 4, name: "Support", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 5, name: "Developer", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 6, name: "HR Department", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 7, name: "Restricted User", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] },
+    { id: 8, name: "Customer", permissions: [{ id: 1, name: "Read" }, { id: 2, name: "Write" }, { id: 3, name: "Delete" }] }
+  ];
+  
