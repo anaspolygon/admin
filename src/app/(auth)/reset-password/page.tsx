c993d68ef0b54469bed5bb1fd9204d53f77cffee
alanas404 <al.anas404040@gmail.com>
2025-03-21 22:13:14 +0600

    reset password screen done

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,8 @@
-// pages/reset-password.tsx
-"use client"
+"use client";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Button, Input } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 export default function ResetPassword() {
   const router = useRouter();
@@ -11,30 +12,56 @@ export default function ResetPassword() {
   const [newPassword, setNewPassword] = useState("");
   const [message, setMessage] = useState("");
 
-interface ResetPasswordResponse {
+  interface ResetPasswordResponse {
     message: string;
-}
+  }
 
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const res = await fetch("/api/auth/reset-password", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token, newPassword }),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token, newPassword }),
     });
 
     const data: ResetPasswordResponse = await res.json();
     setMessage(data.message);
-};
+  };
 
   return (
-    <div>
-      <h2>Set New Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="New password" required />
-        <button type="submit">Reset Password</button>
-      </form>
-      {message && <p>{message}</p>}
+    <div className="bg-[#FFFFFF] w-full h-screen flex items-center justify-center">
+      <div className="w-[500px]">
+        <h2 className="font-lexend-deca text-[36px] leading-[54px] font-bold text-center">Reset your password!</h2>
+        <Input.Password style={{ height: 48 }} onChange={(e) => {
+          setPassword(e.target.value);
+          setPasswordError("")
+        }} className="mt-4" size="large" placeholder="Enter your password" />
+        <Input.Password style={{ height: 48 }} onChange={(e) => {
+          setPassword(e.target.value);
+          setPasswordError("")
+        }} className="mt-4" size="large" placeholder="Enter confirm password" />
+        {passwordError && <p className="text-red-500 mt-2">{passwordError} fdsafsadf</p>}
+         <Button
+          onClick={() => {}}
+          type="primary"
+          size="large"
+          className="w-full mt-4"
+          style={{
+            backgroundColor:"#111111",
+            fontWeight:600,
+            height:48
+          }}
+        >
+          Reset Password <ArrowRightOutlined />
+        </Button>
+        <div className="text-base text-gray-500 font-inter mt-4 flex items-center gap-1">
+          <p>Don’t want to reset your password?</p>
+          <a  href="/login" className="text-[#0070f3]  transition-colors hover:text-gray-900 hover:no-underline font-semibold"> Log in</a>
+        </div>
+      </div>
     </div>
   );
 }
