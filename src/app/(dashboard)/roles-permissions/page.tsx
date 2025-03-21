3f9ad83d3bb49b1a77b3655c1bbb8495721d25fe
alanas404 <al.anas404040@gmail.com>
2025-03-22 01:07:00 +0600

    update roles & permissions

--- src/app/(dashboard)/roles-permissions/page.tsx
+++ src/app/(dashboard)/roles-permissions/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import { useState } from "react";
 import RoleCard from "./components/RoleCard";
-import { Button, Modal } from "antd";
+import { Button, Modal, Select,Input,Space  } from "antd";
 import RoleModal from "./components/RoleModal";
 import {
-  PlusOutlined
+  PlusOutlined,
+  SearchOutlined
 } from '@ant-design/icons';
 import AddNewUserModal from "./components/AddNewUserModal";
 import TableComponent from "../components/TableComponent";
-import { roles } from "./data";
+import { roleOptions, roles, statusOptions } from "./data";
 
 const Page = () => {
- 
+
   const [modal1Open, setModal1Open] = useState(false);
   const [modal2Open, setModal2Open] = useState(false);
   return (
@@ -21,7 +22,7 @@ const Page = () => {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
         {roles.map((role) => (
-          <RoleCard key={role.id} role={role} setModal1Open={setModal1Open}/>
+          <RoleCard key={role.id} role={role} setModal1Open={setModal1Open} />
         ))}
       </div>
       <Modal
@@ -32,13 +33,35 @@ const Page = () => {
         onOk={() => setModal1Open(false)}
         onCancel={() => setModal1Open(false)}
       >
-        <RoleModal/>
+        <RoleModal />
       </Modal>
-      <div className="my-10">
-        <Button onClick={()=> setModal2Open(true)} style={{gap:4,height:40}} type="primary"><PlusOutlined  /> <span className="font-semibold" >Add New User</span></Button>
-        <AddNewUserModal modal2Open={modal2Open} setModal2Open={setModal2Open} />
+      <div className="my-10 flex items-center justify-between">
+        <div className="flex items-center">
+          <h2 className="font-lexend-deca text-lg font-semibold mr-4">All Users</h2>
+          <Select
+            defaultValue="Filter by Status"
+            onChange={() => { }}
+            className="w-[160px] h-9"
+            style={{ height: 40, marginRight: 10 }}
+            options={statusOptions}
+          />
+          <Select
+            defaultValue="Filter by Role"
+            onChange={() => { }}
+            className="w-[160px] h-9"
+            style={{ height: 40 }}
+            options={roleOptions}
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <Space.Compact size="large">
+          <Input size="large" placeholder="Search for users..." prefix={<SearchOutlined />} />
+          </Space.Compact>
+          <Button onClick={() => setModal2Open(true)} style={{ gap: 4, height: 40 }} type="primary"><PlusOutlined /> <span className="font-semibold" >Add New User</span></Button>
+        </div>
       </div>
-      <TableComponent/>
+      <TableComponent />
+      <AddNewUserModal modal2Open={modal2Open} setModal2Open={setModal2Open} />
     </div>
   );
 };
