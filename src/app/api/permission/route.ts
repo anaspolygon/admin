af09d012e82179a5382922e856fff86a863e54af
anaspolygon <al.anas@polygontech.xyz>
2025-04-10 12:12:58 +0600

    permission created

---
+++ src/app/api/permission/route.ts
@@ -0,0 +1,39 @@
+import { Permission } from "@/app/entities/Permission";
+import { getDataSource } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest) {
+   try {
+    const dataSource = await getDataSource();
+    const permissionRepository = dataSource.getRepository(Permission);
+
+    const permissions = [
+        'user:create',
+        'user:read',
+        'user:update',
+        'user:delete',
+        'role:create',
+        'role:read',
+        'role:update',
+        'role:delete',
+      ];
+
+    for (const permissionName of permissions) {
+        const exists = await permissionRepository.findOne({
+            where: { name: permissionName },
+        });
+
+        if(!exists){
+            const permission = permissionRepository.create({ name: permissionName });
+            await permissionRepository.save(permission);
+            console.log(`✅ Inserted permission: ${permissionName}`);
+        }
+    }
+
+    await dataSource.destroy();
+    return NextResponse.json({ message: "Permissions seeded successfully" }, { status: 200 });
+   } catch (error) {
+    console.error('Permission seed error:', error);
+    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+   }
+}
