0ce540db234f2d1f9bb24a1f2832cc9a03cf0d29
anaspolygon <al.anas@polygontech.xyz>
2025-04-13 16:19:54 +0600

    update

--- src/app/(dashboard)/roles-permissions/components/AddNewRoleModal.tsx
+++ src/app/(dashboard)/roles-permissions/components/AddNewRoleModal.tsx
@@ -3,6 +3,7 @@ import { ColorPicker, Space } from "antd";
 import type { ColorPickerProps, GetProp } from "antd";
 import { useEffect, useMemo, useState } from "react";
 import { CheckOutlined } from "@ant-design/icons";
+import toast, { Toaster } from "react-hot-toast";
 
 type Color = Extract<
   GetProp<ColorPickerProps, "value">,
@@ -36,6 +37,8 @@ const AddNewRoleModal = ({
   const [name, setName] = useState<string>("");
   const [loading, setLoading] = useState(false);
 
+  const notify = () => toast.success("Role created successfully");
+
   const addPermission = (permission: any) => {
     if (permissionIds.includes(permission.id)) {
       setPermissionIds((prev) => prev.filter((id) => id !== permission.id));
@@ -50,17 +53,19 @@ const AddNewRoleModal = ({
       const res = await fetch("/api/role", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name,permissionIds }),
+        body: JSON.stringify({ name, permissionIds }),
       });
 
       const data = await res.json();
 
       if (res.ok) {
+        setLoading(false);
+        notify();
+        setModal3Open(false);
       } else {
         console.log(data);
         setLoading(false);
       }
-
     } catch (error) {
       setLoading(false);
     }
@@ -69,58 +74,62 @@ const AddNewRoleModal = ({
   console.log(name);
 
   return (
-    <Modal
-      title="Add a new Role"
-      style={{ top: 20 }}
-      centered
-      open={modal3Open}
-      okText="Create"
-      onOk={() => createRole()}
-      onCancel={() => setModal3Open(false)}
-    >
-      <div className="mb-6">
-        <h2 className="font-inter text-sm text-[#484848] font-semibold mb-1.5">
-          Role Name
-        </h2>
-        <Input
-          style={{ height: 40 }}
-          size="large"
-          placeholder="Enter role name"
-          onChange={(e) => setName(e.target.value)}
+    <>
+      <Modal
+        title="Add a new Role"
+        style={{ top: 20 }}
+        centered
+        open={modal3Open}
+        okText="Create"
+        onOk={() => createRole()}
+        onCancel={() => setModal3Open(false)}
+        confirmLoading={loading}
+      >
+        <div className="mb-6">
+          <h2 className="font-inter text-sm text-[#484848] font-semibold mb-1.5">
+            Role Name
+          </h2>
+          <Input
+            style={{ height: 40 }}
+            size="large"
+            placeholder="Enter role name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-6">
+          <h2 className="font-inter text-sm text-[#484848] font-semibold mb-1.5">
+            Role Color
+          </h2>
+          <Input
+            value={hexString}
+            style={{ height: 40 }}
+            size="large"
+            placeholder="Enter role name"
+          />
+        </div>
+
+        <ColorPicker
+          format={formatHex}
+          value={colorHex}
+          onChange={setColorHex}
+          onFormatChange={setFormatHex}
         />
-      </div>
 
-      <div className="mb-6">
-        <h2 className="font-inter text-sm text-[#484848] font-semibold mb-1.5">
-          Role Color
-        </h2>
-        <Input
-          value={hexString}
-          style={{ height: 40 }}
-          size="large"
-          placeholder="Enter role name"
-        />
-      </div>
-
-      <ColorPicker
-        format={formatHex}
-        value={colorHex}
-        onChange={setColorHex}
-        onFormatChange={setFormatHex}
-      />
-
-      <div className="mt-4 flex items-center gap-2 flex-wrap">
-        {permissions.map((permission, index) => (
-          <button
-            onClick={() => addPermission(permission)}
-            className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-fit-content h-[40px]"
-          >
-            {permissionIds.includes(permission.id) && <CheckOutlined />}
-            <span className="ml-1">{permission.name}</span>
-          </button>
-        ))}
-      </div>
-    </Modal>
+        <div className="mt-4 flex items-center gap-2 flex-wrap">
+          {permissions.map((permission, index) => (
+            <button
+              onClick={() => addPermission(permission)}
+              className="py-2 px-3.5 border font-inter font-sm font-medium text-[#484848] rounded-md w-fit-content h-[40px]"
+            >
+              {permissionIds.includes(permission.id) && <CheckOutlined />}
+              <span className="ml-1">{permission.name}</span>
+            </button>
+          ))}
+        </div>
+      </Modal>{" "}
+      <Toaster position="top-center" />
+    </>
   );
 };
 
